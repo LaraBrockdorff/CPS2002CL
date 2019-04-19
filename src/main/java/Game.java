e155c2f65d9de526014b13465c163408c9470852
Lara <lara.brockdorff.17@um.edu.mt>
2019-04-19 05:46:55 +0200

    adding the creating of map and starting posotion

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Game {
     private int turns;
     private List<Player> players = new ArrayList<Player>();
-    //private Map map = new Map
+    private Map map = new Map();
     private  int n; // number of players
 
 
@@ -26,13 +26,32 @@ public class Game {
 
         setNumberOfPlayers(sc.nextInt());
         //TODO: Add validation
+
+        System.out.println("How many rows (and colums) would you like? ");
+        map.setMapSize(sc.nextInt(),n);
+
     }
 
     public void setNumberOfPlayers(int n){
+        this.n =n;
+        int randX = 0;
+        int randY = 0;
 
         for(int i =0; i<n ; i++){
-            Position newPosition = new Position(0,0);
-            //TODO : add logic re initial postion
+            if(n<5){
+                do {
+                    randX = (int) (Math.random() * ((50 - 5) + 1)) + 5;
+                    randY = (int) (Math.random() * ((50 - 5) + 1)) + 5;
+                }while (map.getTileType(randX,randY) != 'g');
+            }else {
+                do {
+                    randX = (int) (Math.random() * ((50 - 8) + 1)) + 8;
+                    randY = (int) (Math.random() * ((50 - 8) + 1)) + 8;
+                }while (map.getTileType(randX,randY) != 'g');
+            }
+
+            Position newPosition = new Position(randX,randY);
+
             Player newPlayer = new Player( newPosition,i);
             players.add(newPlayer);
         }
